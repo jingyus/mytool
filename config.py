a4a59434128673be63b3dd2ff6b78ff08640aabe
jingyus <382561874@qq.com>
2018-01-11 23:08:59 +0800

    testmysql

---
+++ config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 23:04:12 2018
+
+@author: Administrator
+"""
+
+#coding:utf-8  
+''''' 
+Created on 2016年5月7日 
+ 
+@author: baocheng 
+'''  
+DBHOST = "localhost"  
+DBPORT = 33606  
+DBUSER = "zbc"  
+DBPWD = "123456"  
+DBNAME = "test"  
+DBCHAR = "utf8"  
